ffa242bfcc6e3cad5bd31df670435963f87f3562
pauldtyler <pauldtyler@1837media.com>
2015-08-25 10:10:35 -0400

    changes to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,9 +17,9 @@ var cssminify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var less = require('gulp-less');
 
-var gulp = require('gulp');
+//var gulp = require('gulp');
 var php = require('gulp-connect-php');
-var browserSync = require('browser-sync');
+//var browserSync = require('browser-sync');
 //end thembuilder task references
 
 var notify = require('gulp-notify');
