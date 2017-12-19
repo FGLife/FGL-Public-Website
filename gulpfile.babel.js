5b976578968627794e4505f55385bacb0f00f5c3
KatyClark <katy@katyclark.com>
2017-12-19 11:25:43 -0500

    install modules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -218,6 +218,18 @@ gulp.task('extras', function() {
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 //Serve app
+var gulp = require('gulp');
+var serve = require('gulp-serve');
+
+gulp.task('serve', serve('public'));
+gulp.task('serve-build', serve(['public', 'build']));
+gulp.task('serve-prod', serve({
+  root: ['public', 'build'],
+  port: 80,
+  middleware: function(req, res) {
+    // custom optional middleware
+  }
+}));
 
 gulp.task('serve', ['styles', 'fonts'], function() {
   browserSync({
